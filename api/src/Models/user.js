dd5451598bc8e3979308d421887d76645b8b9155
Andréa.C <155273473+Andie4@users.noreply.github.com>
2025-10-28 14:54:04 +0100

    Update user.js

--- api/src/Models/user.js
+++ api/src/Models/user.js
@@ -10,6 +10,8 @@ const Schema = new mongoose.Schema(
     last_name: { type: String, trim: true },
     password: { type: String, required: true },
     last_login_at: { type: Date, default: Date.now },
+    age: { type: Number, required: true }
+
   },
   { timestamps: true }
 );
