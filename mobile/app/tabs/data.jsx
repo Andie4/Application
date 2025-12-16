7323b7c7e919b12f1d94e582d6d10739404c198b
Andréa.C <155273473+Andie4@users.noreply.github.com>
2025-12-16 22:02:51 +0100

    Ajout de mon tableau de films
    Le tableau me serviras pour l'affichage de mes listes de films : https://fr.react.dev/learn/rendering-lists

---
+++ mobile/app/tabs/data.jsx
@@ -0,0 +1,94 @@
+export const filmsMarvel = [{
+    id: 0,
+    nom: 'Howard… une nouvelle race de héros',
+    date: '1986',
+    realisateur: 'Willard Huyck',
+  },
+  {
+    id: 1,
+    nom: 'Blade',
+    date: '1998',
+    realisateur: 'Stephen Norrington',
+},
+{
+    id: 2,
+    nom: 'X-Men',
+    date: '2000',
+    realisateur: 'Bryan Singer',
+
+},
+{
+    id: 3,
+    nom: 'Blade II',
+    date: '2002',
+    realisateur: 'Guillermo del Toro',
+  },
+{
+    id: 4,
+    nom: 'Spider-Man',
+    date: '2002',
+    realisateur: 'Sam Raimi',
+},
+{
+    id: 5,
+    nom: 'Daredevil',
+    date: '2003',
+    realisateur: 'Mark Steven Johnson',
+}];
+  
+
+//   [1986] Howard… une nouvelle race de héros de Willard Huyck
+// – [1998] Blade de Stephen Norrington
+// – [2000] X-Men de Bryan Singer
+// – [2002] Blade II de Guillermo del Toro
+// – [2002] Spider-Man de Sam Raimi
+// – [2003] Daredevil de Mark Steven Johnson
+// – [2003] X-Men 2 de Bryan Singer
+// – [2003] Hulk d’Ang Lee
+// – [2004] The Punisher de Jonathan Hensleigh
+// – [2004] Spider-Man 2 de Sam Raimi
+// – [2004] Blade : Trinity de David S. Goyer
+// – [2005] Elektra de Rob Bowman
+// – [2005] Les 4 Fantastiques de Tim Story
+// – [2006] X-Men : L’affrontement final de Brett Ratner
+// – [2007] Ghost Rider de Mark Steven Johnson
+// – [2007] Spider-Man 3 de Sam Raimi
+// – [2007] Les 4 Fantastiques et le Surfer d’Argent de Tim Story
+// – [2008] Iron Man de Jon Favreau
+// – [2008] L’Incroyable Hulk de Louis Leterrier
+// – [2008] Punisher – Zone de guerre de Lexi Alexander
+// – [2009] X-Men Origins : Wolverine de Gavin Hood
+// – [2010] Iron Man 2 de Jon Favreau
+// – [2011] Thor de Kenneth Branagh
+// – [2011] X-Men : Le Commencement de Matthew Vaughn
+// – [2011] Captain America : First Avenger de Joe Johnston
+// – [2012] Ghost Rider : L’esprit de vengeance de Mark Neveldine et Brian Taylor
+// – [2012] Avengers de Joss Whedon
+// – [2012] The Amazing Spider-Man de Marc Webb
+// – [2013] Iron Man 3 de Shane Black
+// – [2013] Wolverine : Le Combat de l’Immortel de James Mangold
+// – [2013] Thor : Le Monde des Ténèbres d’Alan Taylor
+// – [2014] Captain America : Le Soldat de l’Hiver d’Anthony et Joe Russo
+// – [2014] The Amazing Spider-Man : Le Destin d’un Héros de Marc Webb
+// – [2014] X-Men : Days of Future Past de Bryan Singer
+// – [2014] Les Gardiens de la Galaxie de James Gunn
+// – [2015] Avengers : L’Ère d’Ultron de Joss Whedon
+// – [2015] Ant-Man de Peyton Reed
+// – [2015] Les 4 Fantastiques de Josh Trank
+// – [2016] Deadpool de Tim Miller
+// – [2016] Captain America : Civil War d’Anthony et Joe Russo
+// – [2016] X-Men : Apocalypse de Bryan Singer
+// – [2016] Doctor Strange de Scott Derrickson
+// – [2017] Logan de James Mangold
+// – [2017] Les Gardiens de la Galaxie Vol. 2 de James Gunn
+// – [2017] Spider-Man : Homecoming de Jon Watts
+// – [2017] Thor : Ragnarok de Taika Waititi
+// – [2018] Black Panther de Ryan Coogler
+// – [2018] Avengers : Infinity War d’Anthony et Joe Russo
+// – [2018] Deadpool 2 de David Leitch
+// – [2018] Ant-Man et la Guêpe de Peyton Reed
+// – [2018] Venom de Ruben Fleischer
+// – [2019] Captain Marvel d’Anna Boden et Ryan Fleck
+// – [2019] Avengers : Endgame d’Anthony et Joe Russo
+// – [2019] X-Men : Dark Phoenix de Simon Kinberg
+// – [2019] Spider-Man : Far From Home de Jon Watts
