26a3959b2b70a1cdf54221f78380272540461176
Andréa.C <155273473+Andie4@users.noreply.github.com>
2025-12-17 22:42:41 +0100

    affichage des films - fonctionnel

--- mobile/app/tabs/marvel.jsx
+++ mobile/app/tabs/marvel.jsx
@@ -1,71 +1,86 @@
-import { Text, View,Ul, Li } from "react-native";
-import { StyleSheet, Button, Alert } from "react-native";
-import { Image } from 'expo-image';
-// import { Link } from 'expo-router';
-// import { listMarvel } from './data'
-import { useState } from 'react';
-
-
-
+import { Text, View, StyleSheet, Alert } from "react-native";
+import { Image } from "expo-image";
+import { useEffect, useState } from "react";
 
 const Marvel = () => {
-  const [films, setFilms] = useState([]) 
-
+  const [films, setFilms] = useState([]);
 
   const fetchList = async () => {
     try {
-      const elementList = await fetch('http://172.20.10.2:3000/film/list',{
-        method: "GET", 
-        headers: { "Content-Type": "application/json" },
-
-      });
-      
-      
+      const elementList = await fetch("http://172.20.10.2:3000/film/list");
       const data = await elementList.json();
-      setFilms(data)
-      console.log(data)
-
-      
-
-    } catch (eror) {
-      Alert.alert("erreur lors de l'affichage des films")
+      setFilms(data);
+    } catch (error) {
+      Alert.alert("Erreur lors de l'affichage des films");
     }
   };
 
+  useEffect(() => {
+    fetchList();
+  }, []);
 
   return (
+    <View style={styles.view}>
+      <Image style={styles.tinyLogo} source={require("../../medias/black-disney.png")} />
 
-      <View style={styles.view}>
-          <Image style={styles.tinyLogo} source={require('../../medias/black-disney.png')}/>
-        <View style={styles.infos}>
-          <Text>Les films du studios Marvel :</Text>   
-           
+      <Text style={styles.title}>Les films du studio Marvel :</Text>
+
+      <Text style={styles.recomandations}>Les films à voir :</Text>
+      <View style={styles.list}>
         {films.map((film) => (
-          <View key={film._id} style={{ marginBottom: 10 }}>
-            <Text>{film.nom}</Text>
-            <Text>{film.date}</Text>
-            <Text>{film.realisateur}</Text>
+          <View key={film._id} style={styles.card}>
+            <Text style={styles.titreFilm}>{film.nom}</Text>
+            <Text>Année de sortie : {film.date}</Text>
+            <Text>Réalisé par : {film.realisateur}</Text>
           </View>
-      ))};
-        </View>
-  
+        ))}
       </View>
-    );
-  };
+      <Text style={styles.archive}>Les films déjà vus:</Text>
+    </View>
+
+    
+  );
+};
 
   const styles = StyleSheet.create({
 
     view:{
-        alignItems: 'center',
+      alignItems: 'center',
+
     },
-    infos: {
+    title:{
+      fontWeight: "bold",
+      fontSize: 25,
+      marginBottom: 50,
+      marginTop: 20
+    },
+    recomandations:{
+      justifyContent: "flex-start",
+      alignItems: "flex-start",
+      marginBottom: 20,
 
-        width:390,
-        height: 400,
-        alignItems:'center',
-        marginBottom: 30,
-        marginTop: 20,
-
+    },
+    titreFilm:{
+      fontWeight: "bold",
+      fontSize: 20,
+      marginBottom: 10,
+    },
+    card:{
+      marginBottom: 10,
+      borderRadius: 10,
+      borderWidth: 2, 
+      // backgroundColor: "#e6f7ff",
+      paddingTop: 10,
+      paddingBottom: 10,
+      paddingLeft: 20,
+      paddingRight: 20,
+      width: 350,
+      borderColor: "white",
+      boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+    },
+    archive:{
+      marginTop: 30,
+      marginBottom: 10,
     },
     tinyLogo: {
       marginTop: 50,
